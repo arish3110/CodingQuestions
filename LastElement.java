32d5689774d16b9bdb41be088ab1426e4e6de32a
arish3110 <arishkkhan234@gmail.com>
2024-04-17 12:08:38 +0530

    first commit

---
+++ LastElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class LastElement {
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt(); //Size of an array
+    
+            int arr[] = new int[n];
+            int len =arr.length;
+            for (int i = 0; i < len; i++) {
+                arr[i] = sc.nextInt();  
+            }
+
+            Arrays.sort(arr);
+            System.out.println("The last element of an array is " + arr[len -1]);
+    }
+}
