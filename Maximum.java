b5718a1c8f490679144a7a8413dd099fb19ac20b
arish3110 <arishkkhan234@gmail.com>
2024-04-17 11:57:17 +0530

    first commit

---
+++ Maximum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Maximum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Size of an array
+
+        int arr[] = new int[n];
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 1st Approach to find maximum number of an array
+        int max = arr[len - 1];
+        for (int i = 0; i < len; i++) {
+            if (max < arr[i])
+                max = arr[i];
+        }
+        System.out.println(max);
+
+        // 2nd approach to find max value in array
+        Arrays.sort(arr);
+        System.out.println(arr[len - 1]);
+    }
+}
---
+++ Minimum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Minimum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Size of an array
+
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 1st Approach to find minimum number of an array
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+
+        // 2nd Approach to find minimum number of an Array
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+
+    }
+}
