17d549c76eb3cba7daa7c602c5bf05027b0ead7e
arish3110 <arishkkhan234@gmail.com>
2024-04-24 16:33:05 +0530

    String Question

--- SumOfFirstCharaString.java
+++ SumOfFirstCharaString.java
@@ -4,7 +4,6 @@ public class SumOfFirstCharaString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-
         int sum = s.charAt(0);
         for (int i = 0; i <= s.length() - 1; i++) {
             if (s.charAt(i) == ' ') {
