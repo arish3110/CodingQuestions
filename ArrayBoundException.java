1072357e7a73c0eb376925218d71020b52811888
arish3110 <arishkkhan234@gmail.com>
2024-04-24 16:13:21 +0530

    ExceptionHandling question

---
+++ ArrayBoundException.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ArrayBoundException {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[7];
+        try{
+             for(int i=0;i<7;i++){
+              a[7]=sc.nextInt();
+            }
+            System.out.println("Hellooo");
+        }
+        catch(ArrayIndexOutOfBoundsException e ){
+            System.out.println("Not printed");
+        }
+       
+    }
+}
