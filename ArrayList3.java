cd17408832704d4d1e9cd929b1840a23cd605edc
arish3110 <arishkkhan234@gmail.com>
2024-05-02 17:25:36 +0530

    Collection Framework Questions(size() method)

---
+++ ArrayList3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList3 {
+    public static void main(String[] args) {
+       List<String> company=new ArrayList<>();
+       company.add("HCL");
+       company.add("RailWorld");
+       company.add("AxisBank");
+       company.add("Meta");
+       company.add("Amazon");
+
+       //size of an arraylist
+        System.out.println("The size of an list is "+ company.size());
+
+    }
+}
